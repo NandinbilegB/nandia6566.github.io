3929afbafa1094e155f313c5dbfba78df063b145
NandinbilegB <159098611+NandinbilegB@users.noreply.github.com>
2024-12-13 13:49:46 +0800

    lab4

--- lab9.java
+++ lab9.java
@@ -1,102 +1,128 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.util.Random;
+import java.util.Scanner;
 
-public class Hangman extends JPanel {
-    private String[] words = {"apple", "banana", "cherry", "date", "grape"};
-    private String selectedWord;
-    private StringBuilder currentWord;
-    private int attemptsLeft = 5;
-    private String guessedLetters = "";
-    private boolean gameOver = false;
+public class Hangman {
 
-    private final StringBuilder wordBuilder = new StringBuilder();
+	private static String[] words = {"terminator", "banana", "computer", "cow", "rain", "water" };
+	private static String word = words[(int) (Math.random() * words.length)];
+	private static String asterisk = new String(new char[word.length()]).replace("\0", "*");
+	private static int count = 0;
 
-    public Hangman() {
-        Random rand = new Random();
-        selectedWord = words[rand.nextInt(words.length)];
-        currentWord = new StringBuilder("_".repeat(selectedWord.length()));
-        setPreferredSize(new Dimension(500, 400));
-        setBackground(Color.WHITE);
-        
-        addKeyListener(new KeyAdapter() {
-            public void keyPressed(KeyEvent e) {
-                if (gameOver) return;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-                char guess = Character.toLowerCase(e.getKeyChar());
-                if (Character.isLetter(guess) && !guessedLetters.contains(String.valueOf(guess))) {
-                    guessedLetters += guess;
-                    if (selectedWord.contains(String.valueOf(guess))) {
-                        updateWordDisplay(guess);
-                    } else {
-                        attemptsLeft--;
-                    }
-                    if (attemptsLeft == 0) {
-                        gameOver = true;
-                    }
-                    repaint();
-                }
-            }
-        });
-        setFocusable(true);
-    }
+		while (count < 7 && asterisk.contains("*")) {
+			System.out.println("Guess any letter in the word");
+			System.out.println(asterisk);
+			String guess = sc.next();
+			hang(guess);
+		}
+		sc.close();
+	}
 
+	public static void hang(String guess) {
+		String newasterisk = "";
+		for (int i = 0; i < word.length(); i++) {
+			if (word.charAt(i) == guess.charAt(0)) {
+				newasterisk += guess.charAt(0);
+			} else if (asterisk.charAt(i) != '*') {
+				newasterisk += word.charAt(i);
+			} else {
+				newasterisk += "*";
+			}
+		}
 
-    private void updateWordDisplay(char guess) {
-        for (int i = 0; i < selectedWord.length(); i++) {
-            if (selectedWord.charAt(i) == guess) {
-                currentWord.setCharAt(i, guess);
-            }
-        }
-    }
+		if (asterisk.equals(newasterisk)) {
+			count++;
+			hangmanImage();
+		} else {
+			asterisk = newasterisk;
+		}
+		if (asterisk.equals(word)) {
+			System.out.println("Correct! You win! The word was " + word);
+		}
+	}
 
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-       
-        g.setFont(new Font("Arial", Font.PLAIN, 24));
-        g.drawString("Word: " + currentWord, 20, 50);
-        g.drawString("Guessed Letters: " + guessedLetters, 20, 80);
-        g.drawString("Attempts Left: " + attemptsLeft, 20, 110);
-
-      
-        drawHangman(g);
-
-        
-        if (gameOver) {
-            if (attemptsLeft == 0) {
-                g.drawString("Game Over! The word was: " + selectedWord, 20, 150);
-            } else {
-                g.drawString("You Won!", 20, 150);
-            }
-        }
-    }
-
-    private void drawHangman(Graphics g) {
-    
-        int x = 200, y = 150;
-        switch (attemptsLeft) {
-            case 4: g.drawLine(x, y, x, y + 50); break; 
-            case 3: g.drawLine(x, y + 50, x - 20, y + 80); break; 
-            case 2: g.drawLine(x, y + 50, x + 20, y + 80); break; 
-            case 1: g.drawLine(x, y + 20, x - 30, y + 40); break; 
-            case 0: g.drawLine(x, y + 20, x + 30, y + 40); break; 
-        }
-
-        
-        g.drawLine(x - 50, y, x + 50, y);  
-        g.drawLine(x - 50, y, x - 50, y + 100);  
-        g.drawLine(x + 50, y, x + 50, y + 30);  
-    }
-
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Hangman Game");
-        Hangman gamePanel = new Hangman();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(gamePanel);
-        frame.pack();
-        frame.setVisible(true);
-    }
+	public static void hangmanImage() {
+		if (count == 1) {
+			System.out.println("Wrong guess, try again");
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println("___|___");
+			System.out.println();
+		}
+		if (count == 2) {
+			System.out.println("Wrong guess, try again");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("___|___");
+		}
+		if (count == 3) {
+			System.out.println("Wrong guess, try again");
+			System.out.println("   ____________");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   | ");
+			System.out.println("___|___");
+		}
+		if (count == 4) {
+			System.out.println("Wrong guess, try again");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("___|___");
+		}
+		if (count == 5) {
+			System.out.println("Wrong guess, try again");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |           |");
+			System.out.println("   |           |");
+			System.out.println("   |");
+			System.out.println("___|___");
+		}
+		if (count == 6) {
+			System.out.println("Wrong guess, try again");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |           |");
+			System.out.println("   |           |");
+			System.out.println("   |          / \\ ");
+			System.out.println("___|___      /   \\");
+		}
+		if (count == 7) {
+			System.out.println("GAME OVER!");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |          _|_");
+			System.out.println("   |         / | \\");
+			System.out.println("   |          / \\ ");
+			System.out.println("___|___      /   \\");
+			System.out.println("GAME OVER! The word was " + word);
+		}
+	}
 }
